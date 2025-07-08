7cb8ecc9fc38562e84f094ed4c08395abf50de87
gopalreddypulikanti <gopalpulikanti33@gmail.com>
2025-07-08 18:29:48 +0530

    new file is the adding

--- application.py
+++ application.py
@@ -1,21 +1,24 @@
 class Father:
-  def add(x, y):
+  def add(self, x, y):
     return x + y
 
 
 class Mother(Father):
-  def sub(x, y):
+  def sub(self, x, y):
     return x - y
 
 class Brother(Mother):
-  def mul(x, y):
+  def mul(self, x, y):
     return x * y
 
 class Sister(Brother):
-  def div(x, y):
+  def div(self, x, y):
     return x / y
 
 inst_obj = Sister()
 print(inst_obj.add(20, 30))
 print(inst_obj.sub(30, 20))
 print(inst_obj.mul(15, 15))
+
+
+print("hello")
